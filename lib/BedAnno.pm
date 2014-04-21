e8959e9c07d17e85d3c9c810aeace73e06f8f308
liutao <liutao@genomics.cn>
2014-04-21 16:27:35 +0800

    change a bit

--- lib/BedAnno.pm
+++ lib/BedAnno.pm
@@ -6756,6 +6756,8 @@ liutao, E<lt>liutao@genomics.cnE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Please see LICENSE for detail
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
 
 =cut
