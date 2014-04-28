b3f9efa8788316a17c746c6842d3e4ff6191b9dd
liutao <liutao@genomics.cn>
2014-04-28 09:22:18 +0800

    update to 0.5.8

--- lib/BedAnno.pm
+++ lib/BedAnno.pm
@@ -8,13 +8,13 @@ use Time::HiRes qw(gettimeofday tv_interval);
 
 use Tabix;
 
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 
 =head1 NAME
 
 BedAnno - Perl module for annotating variation depend on the BED +1 format database.
 
-=head2 VERSION v0.57
+=head2 VERSION v0.58
 
 From version 0.32 BedAnno will change to support CG's variant shell list
 and use ncbi annotation release 104 as the annotation database
